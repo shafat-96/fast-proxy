c8bc845e2810fbf973f6285ff2150abb0fbd045e
shafat-96 <animeexost61@gmail.com>
2025-08-18 00:02:26 +0600

    Update domainTemplates.js

--- domainTemplates.js
+++ domainTemplates.js
@@ -1421,6 +1421,24 @@ export const domainTemplates = [
     }
   },
 
+ {
+    pattern: /hollymoviehdcdn04\.xyz$/i,
+    headers: {
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0',
+      'accept': '*/*',
+      'accept-language': 'en-US,en;q=0.5',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'cross-site',
+    },
+    headersFn: (url) => {
+      return {
+        'origin': 'https://pkaystream.cc',
+        'referer': 'https://pkaystream.cc/',
+      };
+    }
+  },
+
   // General default template
   {
     pattern: /.*/,
