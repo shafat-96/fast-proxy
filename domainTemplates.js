bf49f1af040b3d926d1b4b18951065f862156933
shafat-96 <animeexost61@gmail.com>
2025-08-23 23:20:34 +0600

    Update domainTemplates.js

--- domainTemplates.js
+++ domainTemplates.js
@@ -1346,6 +1346,24 @@ export const domainTemplates = [
     }
   },
 
+    {
+    pattern: /\.netmagcdn\.com$/i,
+    headers: {
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0',
+      'accept': '*/*',
+      'accept-language': 'en-US,en;q=0.5',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'cross-site',
+    },
+    headersFn: (url) => {
+      return {
+        'origin': 'https://megacloud.blog',
+        'referer': 'https://megacloud.blog/',
+      };
+    }
+  },
+
   // ss-cdn*.top
   {
     pattern: /\.ss-cdn.*\.top$/i,
